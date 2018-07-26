9991bd5462efb9aa3a7bd5c2a6ae36929c2a7c93
phantombeast <rajpalpulkit12@gmail.com>
2018-07-27 00:53:48 +0530

    bcyptjs

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const router =express.Router();
+const gravatar=require('gravatar')
+const bycrypt=require('bcryptjs')
 //Load USer Model
 
 const User=require('../../models/User');
@@ -12,17 +14,36 @@ router.get('/test',(req,res)=>{
     res.json({msg:"Users works"})
 });
 
-router.post('/resgister',(req,res)=>{
+router.post('/register',(req,res)=>{
 User.findOne({email:req.body.email})
     .then(user=>{
         if(user){
             return res.status(400).json({email:'Email already eists'})
         }
         else {
+            const avatar=gravatar.url(req.body.email,{
+                s:'200',
+                r:'pg',
+                d:'mm'
+
+            });
+
             const newUser=new User({
                 name:req.body.name,
-                email:req.body.password,
+                email:req.body.email,
+                avatar,
+                password:req.body.password
 
+            });
+
+            bycrypt.genSalt(10,(err,salt)=>{
+                bycrypt.hash(newUser.password,salt,(error,hash)=>{
+                    if(err) throw err;
+                    newUser.password=hash;
+                    newUser.save()
+                        .then(user=>res.json(user))
+                        .catch(err=>console.log(err))
+                })
             })
 
         }
