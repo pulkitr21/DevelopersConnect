f59d14d73da5a2106d9f3b7c48f62a97be8ffebe
phantombeast <rajpalpulkit12@gmail.com>
2018-07-27 00:10:46 +0530

    post

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express=require('express');
-
 const router =express.Router();
+//Load USer Model
+
+const User=require('../../models/User');
 
 //@route GET    api/users/test
 //@desc Tests users request
@@ -8,6 +10,23 @@ const router =express.Router();
 
 router.get('/test',(req,res)=>{
     res.json({msg:"Users works"})
+});
+
+router.post('/resgister',(req,res)=>{
+User.findOne({email:req.body.email})
+    .then(user=>{
+        if(user){
+            return res.status(400).json({email:'Email already eists'})
+        }
+        else {
+            const newUser=new User({
+                name:req.body.name,
+                email:req.body.password,
+
+            })
+
+        }
+    })
 })
 
 module.exports=router;
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express=require('express');
 const mongoose=require('mongoose')
+const bodyParser=require('body-parser');
+
 const app=express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json());
+
 
 const users=require('./routes/api/users')
 const profile=require('./routes/api/profile')
